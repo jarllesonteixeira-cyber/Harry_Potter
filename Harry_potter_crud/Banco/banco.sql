a7bb4d310e56edd9665531de7fb06460a7268ee5
jarllesonteixeira-cyber <jarlleson.teixeira@aluno.ce.gov.br>
2026-06-13 11:08:19 -0300

    Delete Harry_potter_crud directory

--- Harry_potter_crud/Banco/banco.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE feiticos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    tipo VARCHAR(50) NOT NULL,
-    movimento_varinha VARCHAR(150),
-    efeito VARCHAR(255) NOT NULL,
-    nivel_dificuldade VARCHAR(20) NOT NULL,
-    proibido TINYINT(1) DEFAULT 0
-);
